028e978078fbea92e75c572a2eb1eaeb8d83812d
zhangshuai <310966164@qq.com>
2020-05-29 10:23:13 +0800

    测试

---
+++ QQserver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/29 10:06
+# @Author  : zhang shuai
+# @Email   : 310966164@qq.com
+# @File    : QQserver.py
+# @Software: PyCharm
+
+
+if __name__ == '__main__':
+    pass
---
+++ loginUI.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/29 10:22
+# @Author  : zhang shuai
+# @Email   : 310966164@qq.com
+# @File    : loginUI.py
+# @Software: PyCharm
